ecc6a2717afbc70b2bf08b5a18b115a74d7fd607
skymonil <monilparikh11@gmail.com>
2026-01-30 18:21:24 +0530

    Changed Port in inventory-service

--- services/inventory-service/src/redis/client.js
+++ services/inventory-service/src/redis/client.js
@@ -3,7 +3,8 @@ const Redis = require("ioredis");
 
 const redis = new Redis({
   host: process.env.REDIS_HOST || "localhost",
-  redisPort: Number(process.env.REDIS_PORT || 6379),
+  
+  port: Number(process.env.REDIS_PORT || 6379),
 
   // ✅ for prod (ElastiCache usually has auth + TLS)
   password: process.env.REDIS_PASSWORD || undefined,
