609ac4bd30f04581d0e0e2499ac821a5b3ece544
skymonil <monilparikh11@gmail.com>
2026-02-04 12:57:13 +0530

    Change-user-service-cd.yaml

--- services/user-service/src/app.js
+++ services/user-service/src/app.js
@@ -21,7 +21,7 @@ app.use(requestIdMiddleware);
 
 // Health check (important for Kubernetes)
 app.get("/health", (req, res) => {
-  res.status(200).json({ status: "UP Version 5.2" });
+  res.status(200).json({ status: "UP Version 5.3" });
 });
 
 // Routes
