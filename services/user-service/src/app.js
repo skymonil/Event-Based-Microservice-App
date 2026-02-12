b267756e1b733e746bacf95175d7a72c307140b3
skymonil <monilparikh11@gmail.com>
2026-02-12 06:52:38 +0530

    Testing CICD 1

--- services/user-service/src/app.js
+++ services/user-service/src/app.js
@@ -37,7 +37,7 @@ app.use(requestIdMiddleware);
 
 // Health check (important for Kubernetes)
 app.get("/health", (req, res) => {
-  res.status(200).json({ status: "UP Version 5.39" });
+  res.status(200).json({ status: "UP Version 5.40" });
 });
 
 // Routes
