69535f543083dd6d68a5b36d2b280ff83efa217e
skymonil <monilparikh11@gmail.com>
2026-02-13 10:29:50 +0530

    Changed app.js

--- services/user-service/src/index.js
+++ services/user-service/src/index.js
@@ -6,7 +6,7 @@ const gracefulShutdown = require('http-graceful-shutdown');
 const server = http.createServer(app);
 
 server.listen(config.port, () => {
-    logger.info(`User ServiceV5.49 running on port ${config.port}`);
+    logger.info(`User ServiceV5.50 running on port ${config.port}`);
 });
 
 gracefulShutdown(server, {
