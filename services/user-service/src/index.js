d3dbb209b155df8d811c8d250db100f38549adab
skymonil <monilparikh11@gmail.com>
2026-04-07 21:43:58 +0530

    Added Version in reusable-node-ci.yaml

--- services/user-service/src/index.js
+++ services/user-service/src/index.js
@@ -9,7 +9,7 @@ const gracefulShutdown = require("http-graceful-shutdown");
 const server = http.createServer(app);
 
 server.listen(config.port, () => {
-	logger.info(`User ServiceV5.72  running on port ${config.port}`);
+	logger.info(`User ServiceV5.73  running on port ${config.port}`);
 });
 
 gracefulShutdown(server, {
