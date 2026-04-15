c714131b658b186be62583ee50437f542d137682
skymonil <monilparikh11@gmail.com>
2026-04-15 18:19:36 +0530

    Change index.js

--- services/user-service/src/middleware/validate.middleware.js
+++ services/user-service/src/middleware/validate.middleware.js
@@ -1,20 +1,40 @@
 // middleware/validate.middleware.js
-const { AppError } = require("@my-app/common");
-const validate = (schema) => (req, _res, next) => {
-	const { error } = schema.validate(req.body);
-	if (error) {
-		const message = error.details.map((i) => i.message).join(",");
 
-		// 🟢 PASS TO ERROR HANDLER (Don't res.json here!)
-		return next(
-			new AppError({
-				title: "Bad Request",
-				status: 400,
-				detail: message,
-			}),
-		);
-	}
-	next();
+const { AppError } = require("@my-app/common");
+
+const validate =
+  (schema, source = "body") =>
+  (req, _res, next) => {
+
+    const data =
+      source === "params"
+        ? req.params
+        : source === "query"
+        ? req.query
+        : req.body;
+
+    const { error } =
+      schema.validate(data);
+
+    if (error) {
+
+      const message =
+        error.details
+          .map(i => i.message)
+          .join(", ");
+
+      return next(
+        new AppError({
+          status: 400,
+          title: "Bad Request",
+          detail: message,
+          type: "/problems/validation-error"
+        })
+      );
+
+    }
+
+    next();
 };
 
 module.exports = validate;
