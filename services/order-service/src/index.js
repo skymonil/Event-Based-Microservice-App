7a1a5a442a1f0ef431883961ca7e53156af125c5
skymonil <monilparikh11@gmail.com>
2026-05-01 19:07:37 +0530

    Change order-service version

--- services/order-service/src/index.js
+++ services/order-service/src/index.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 	await connectProducer();
 	await startConsumer();
 	server.listen(config.port, () => {
-		logger.info(`Order Service 5.85 running on port ${config.port}`);
+		logger.info(`Order Service 5.86 running on port ${config.port}`);
 	});
 })();
 
