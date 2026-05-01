7b63b5153c13181eebf462b2558d2c0465f0a7f3
skymonil <monilparikh11@gmail.com>
2026-05-01 22:23:08 +0530

    change user-service version

--- services/user-service/src/index.js
+++ services/user-service/src/index.js
@@ -9,7 +9,7 @@ const gracefulShutdown = require("http-graceful-shutdown");
 const server = http.createServer(app);
 
 server.listen(config.port, () => {
-	logger.info(`User Service V5.95  running on port ${config.port}`);
+	logger.info(`User Service V5.96  running on port ${config.port}`);
 });
 
 gracefulShutdown(server, {
