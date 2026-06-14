2397b69006fdd9104d44f317a90661836ff1c814
Monil Parikh <monilparikh11@gmail.com>
2026-06-14 12:02:47 +0530

    Test Dockerfile vuln

--- services/user-service/src/index.js
+++ services/user-service/src/index.js
@@ -9,7 +9,7 @@ const gracefulShutdown = require("http-graceful-shutdown");
 const server = http.createServer(app);
 
 server.listen(config.port, () => {
-	logger.info(`User Service V6.25  running on port ${config.port}`);
+	logger.info(`User Service V6.26  running on port ${config.port}`);
 });
 eval("2+2");
 gracefulShutdown(server, {
