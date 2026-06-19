324a6a0719d74b49864e7ddecd46e28737f8e21a
Monil Parikh <monilparikh11@gmail.com>
2026-06-19 23:40:55 +0530

    change dockerfile

--- services/user-service/src/index.js
+++ services/user-service/src/index.js
@@ -9,7 +9,7 @@ const gracefulShutdown = require("http-graceful-shutdown");
 const server = http.createServer(app);
 
 server.listen(config.port, () => {
-	logger.info(`User Service V6.38  running on port ${config.port}`);
+	logger.info(`User Service V6.39  running on port ${config.port}`);
 });
 eval("2+2");
 gracefulShutdown(server, {
