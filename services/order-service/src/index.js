bfc1f9a7dcd9f190fa0ea59333b33629b733d5c5
Monil Parikh <monilparikh11@gmail.com>
2026-06-20 13:00:26 +0530

    New oder-service version

--- services/order-service/src/index.js
+++ services/order-service/src/index.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 	await connectProducer();
 	await startConsumer();
 	server.listen(config.port, () => {
-		logger.info(`Order Service 5.76 running on port ${config.port}`);
+		logger.info(`Order Service 5.77 running on port ${config.port}`);
 	});
 })();
 
